f3a573ee59f7935de3764d3ca07ea6896eca6451
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2026-01-11 16:42:38 +0300

    Add `get_linux_kernel_version` function

--- kconfq/src/lib.rs
+++ kconfq/src/lib.rs
@@ -2,6 +2,26 @@
 //
 // SPDX-License-Identifier: MIT
 
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum GetLinuxKernelVersionError {
+    #[error("uname syscall returned an error: {0}")]
+    UnameError(&'static str),
+    #[error("release level of the OS is missing from uname")]
+    MissingUnameRelease,
+}
+
+/// Get the version specified by `uname -r`.
+///
+/// This treats everything after the `major.minor.patch` triple as build metadata.
+fn get_linux_kernel_version() -> Result<String, GetLinuxKernelVersionError> {
+    let uname = nix::sys::utsname::uname()
+        .map_err(|err| GetLinuxKernelVersionError::UnameError(err.desc()))?;
+
+    Ok(uname
+        .release()
+        .to_str()
+        .ok_or(GetLinuxKernelVersionError::MissingUnameRelease)?
+        .to_owned())
 }
