d708a3a652be8ae4fb736485140efeece16d3aaa
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2026-01-15 16:44:49 +0300

    Use kconfq from this repository in example flake

--- examples/libkconfq-simple/flake.nix
+++ examples/libkconfq-simple/flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     kconfq = {
-      url = "github:synalice/kconfq";
+      url = "../..";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
