1ce6e045927f86208b83b49ce34deb3fa2b82612
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2026-01-15 17:58:08 +0300

    Rename capi::find_line to capi::kconfq_find_line

--- src/capi.rs
+++ src/capi.rs
@@ -242,7 +242,7 @@ pub unsafe extern "C" fn kconfq_locate_config(
 /// `entry_name == "CONFIG_COMPILE_TEST"` may return\
 ///  `# CONFIG_COMPILE_TEST is not set`
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn find_line(
+pub unsafe extern "C" fn kconfq_find_line(
     entry_name: *const c_char,
     out_line: *mut *mut c_char,
     out_err: *mut *mut KconfqError,
