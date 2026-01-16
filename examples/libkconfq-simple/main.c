979d624aa0d240923b7edd8366c3f27d984f4aa0
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2026-01-16 21:51:40 +0300

    Update C example

--- examples/libkconfq-simple/main.c
+++ examples/libkconfq-simple/main.c
@@ -35,6 +35,18 @@ int main(void) {
 
   printf("Line: %s\n", line);
 
+  const char *entry_value = NULL;
+  res = kconfq_find_value(config, "CONFIG_CC_VERSION_TEXT", &entry_value);
+  if (res != KCONFQ_RESULT_SUCCESS) {
+    fprintf(stderr, "Error: %s\n", kconfq_result_strerror(res));
+    exit_status = EXIT_FAILURE;
+    goto cleanup4;
+  }
+
+  printf("Entry value: %s\n", entry_value);
+
+cleanup4:
+  kconfq_free_string(entry_value);
 cleanup3:
   kconfq_free_string(line);
 cleanup2:
